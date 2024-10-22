e0aa03d26166ba83d8ecf18e1c02af1d2ef7799a
AngelGabrielG <angel.gaitan.ev@gmail.com>
2024-10-22 20:20:59 -0300

    added footer and a little change in header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
+import { Subscription, timer } from 'rxjs';
+import { filter } from 'rxjs/operators';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -15,7 +18,41 @@ import { HttpClient } from '@angular/common/http';
   styleUrl: './app.component.scss',
   animations: [fader]
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  showFooter = false;
+  private footerTimer: Subscription | null = null;
+  private navigationSubscription: Subscription;
+
+  constructor(private router: Router) {
+    this.navigationSubscription = this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.resetFooterTimer();
+    });
+  }
+
+  ngOnInit() {
+    this.resetFooterTimer();
+  }
+
+  ngOnDestroy() {
+    if (this.footerTimer) {
+      this.footerTimer.unsubscribe();
+    }
+    if (this.navigationSubscription) {
+      this.navigationSubscription.unsubscribe();
+    }
+  }
+
+  resetFooterTimer() {
+    this.showFooter = false;
+    if (this.footerTimer) {
+      this.footerTimer.unsubscribe();
+    }
+    this.footerTimer = timer(2000).subscribe(() => {
+      this.showFooter = true;
+    });
+  }
 
   prepareRoute(outlet: RouterOutlet){
     return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
