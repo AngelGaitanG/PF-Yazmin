65f9d2f17d07ab5d010dea259cc1ce55299230e0
AngelGabrielG <angel.gaitan.ev@gmail.com>
2024-11-12 08:02:49 -0300

    deleted a view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { HomeComponent } from './features/home/home.component';
 import { BlogComponent } from './features/blog/blog.component';
 import { IntroductionComponent } from './features/introduction/introduction.component';
 import { GalleryComponent } from './features/gallery/gallery.component';
-import { CartitaComponent } from './shared/components/cartita/cartita.component';
 
 export const routes: Routes = [
     {
@@ -17,9 +16,6 @@ export const routes: Routes = [
     },
     {
         path: 'gallery', component: GalleryComponent, data: { animation: 'gallery' }
-    },
-    {
-        path: 'cartita', component: CartitaComponent, data: { animation: 'cartita' }
     },
     {
         path: '', redirectTo: 'home', pathMatch: 'full'
